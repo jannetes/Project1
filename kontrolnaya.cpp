47b279bc8340286840bcf569fb798ac9305ce681
jannetes <103538762+jannetes@users.noreply.github.com>
2022-05-14 11:53:39 +0300

    Add files via upload

---
+++ kontrolnaya.cpp
@@ -0,0 +1,40 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+ int main()
+  {
+      setlocale(LC_ALL, "Russian");
+
+      cout << "Введите количествo чисел: " << endl;
+      int size;
+      cin >> size;
+
+      int* a = new int[size];
+
+      cout << "Введите числа: " << endl;
+      for (size_t i = 0; i < size; i++)
+      {
+        cin >> a[i];
+      }
+ 
+      int min = a[0];
+
+       for (int i = 1; i < size; i++)
+
+       {
+
+         if (min > a[i])
+
+         {
+
+          min = a[i];
+
+         }
+
+       }
+
+ cout << "Минимальный элемент массива: " << min;
+
+  }
